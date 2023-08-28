970bdd62704de5b67677a035f368bddc0644e1ab
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-08-28 19:17:05 +0300

    test

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,8 @@ module.exports = ({ env }) => ({
         enabled: true,
         config: {
             apiToken: process.env.VERCEL_TOKEN,
-deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+            deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
       }
   },
 });
