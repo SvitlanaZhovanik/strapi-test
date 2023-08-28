4f0fcb1700138ca19a772cc291c6fed2733b05f6
Svitlana Zhovanik <lanasvetcat@gmail.com>
2023-08-28 19:13:11 +0300

    add deploy hook

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,8 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
         enabled: true,
         config: {
-          apiToken: process.env.VERCEL_TOKEN,
+            apiToken: process.env.VERCEL_TOKEN,
+deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
       }
   },
 });
